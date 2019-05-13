d00feed9d3d5c1da8aaa79d7c421d547ceb72a10
Phuong <phuong.tran@jvncomm.com>
2019-05-13 09:11:56 -0400

    Init

---
+++ Navigation_Data_Updater/Navigation_Data_Updater.cpp
@@ -0,0 +1,9 @@
+// Navigation_Data_Updater.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
